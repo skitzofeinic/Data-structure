f7741b3f3722ed7550792e9513a9313697431aea
Nguyen Anh Le <anhle261137@gmail.com>
2023-11-25 19:22:10 +0100

    hash_table WIP

--- hash_tables/array.c
+++ hash_tables/array.c
@@ -10,17 +10,17 @@ struct array {
 
 struct array *array_init(unsigned long initial_capacity) {
     struct array *a = malloc(sizeof(struct array));
-    if (a == NULL) return NULL;
+    if (!a) return NULL;
 
     a->element = malloc(sizeof(int) * initial_capacity);
-    if (a->element == NULL) {
+    if (!a->element) {
         array_cleanup(a);
         return NULL;
     }
 
     a->size = 0;
     a->capacity = initial_capacity;
-
+    
     return a;
 }
 
@@ -30,22 +30,16 @@ void array_cleanup(struct array *a) {
 }
 
 int array_get(const struct array *a, unsigned long index) {
-    return (a == NULL || index > a->size) ? -1 : a->element[index];
+    return (!a || index > a->size) ? -1 : a->element[index];
 }
 
-/* Note: Although this operation might require the array to be resized and
- * copied, in order to make room for the added element, it is possible to do
- * this in such a way that the amortized complexity is still O(1).
- * Make sure your code is implemented in such a way to guarantee this. */
 int array_append(struct array *a, int elem) {
-    if (a == NULL) return 1;
+    if (!a) return 1;
 
     if (array_size(a) >= a->capacity) {
-        unsigned long new_capacity = a->capacity * 2;
-        a->capacity = new_capacity;
-
-        int *new_element = realloc(a->element, sizeof(int) * new_capacity);
-        if (new_element == NULL) return 1;
+        a->capacity *= 2;
+        int *new_element = realloc(a->element, sizeof(int) * a->capacity);
+        if (!new_element) return 1;
         a->element = new_element;
     }
     a->element[a->size++] = elem;
@@ -54,5 +48,5 @@ int array_append(struct array *a, int elem) {
 }
 
 unsigned long array_size(const struct array *a) {
-    return a->size;
+    return (a) ? a->size : NULL;
 }
--- hash_tables/hash_table.c
+++ hash_tables/hash_table.c
@@ -10,7 +10,6 @@ struct table {
     struct node **array;
     /* The function used for computing the hash values in this table */
     unsigned long (*hash_func)(const unsigned char *);
-
     /* Maximum load factor after which the table array should be resized */
     double max_load_factor;
     /* Capacity of the array used to index the table */
@@ -19,50 +18,116 @@ struct table {
     unsigned long load;
 };
 
-/* Note: This struct should be a *strong* hint to a specific type of hash table
- * You may implement other options, if you can build them in such a way they
- * pass all tests. However, the other options are generally harder to code. */
 struct node {
     /* The string of characters that is the key for this node */
     char *key;
     /* A resizing array, containing the all the integer values for this key */
     struct array *value;
-
     /* Next pointer */
     struct node *next;
 };
-
-/* ... SOME CODE MISSING HERE ... */
 
 struct table *table_init(unsigned long capacity,
                          double max_load_factor,
                          unsigned long (*hash_func)(const unsigned char *)) {
 
-    /* Note: The 'array' member of struct table is a pointer to a block of
-     * memory that contains pointers to struct nodes. Make sure that the struct
-     * node pointers in this block are all set to NULL. A good way to do this is
-     * with the calloc() function. Check the manual page 'man calloc' for
-     * its usage. */
+    struct table *t = malloc(sizeof(struct table));
+    if (!t) return NULL;
 
-    /* ... SOME CODE MISSING HERE ... */
+    t->array = calloc(capacity, (sizeof(struct node *)));
+    if (!t->array) {
+        table_cleanup(t);
+        return NULL;
+    }
+
+    t->hash_func = hash_func;
+    t->max_load_factor = max_load_factor;
+    t->capacity = capacity;
+    t->load = 0;
+
+    return t;
 }
 
 int table_insert(struct table *t, const char *key, int value) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (!t || !key) return 1;
+    
+    unsigned long index = t->hash_func((const unsigned char *)key) % t->capacity;
+
+    struct node *current = t->array[index];
+    while (current) {
+        if (strcmp(current->key, key) == 0) {
+            return array_append(current->value, value) ? 1 : 0;
+        }
+        current = current->next;
+    }
+
+    return 0;
 }
 
 struct array *table_lookup(const struct table *t, const char *key) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (!t || !key) return NULL;
+    
+    unsigned long index = t->hash_func((const unsigned char *)key) % t->capacity;
+    
+    struct node *current = t->array[index];
+    while (current) {
+        if (strcmp(current->key, key) == 0) return (struct array *)current->key;
+        current = current->next;
+    }
+
+    return NULL;
 }
 
 double table_load_factor(const struct table *t) {
-    /* ... SOME CODE MISSING HERE ... */
+    return (!t || t->capacity == 0) ? -1.0 : (double)t->load / (double)t->capacity;
 }
 
 int table_delete(struct table *t, const char *key) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (!t || !key) return 1;
+
+    unsigned long index = t->hash_func((const unsigned char *)key) % t->capacity;
+
+    struct node *current = t->array[index];
+    struct node *prev = NULL;
+
+    while (current) {
+        if (strcmp(current->key, key) == 0) {
+            if (prev) {
+                prev->next = current->next;
+            } else {
+                t->array[index] = current->next;
+            }
+
+            free(current->key);
+            array_cleanup(current->value);
+            free(current);
+            t->load--;
+
+            return 0;
+        }
+
+        prev = current;
+        current = current->next;
+    }
+
+    return 1;
 }
 
 void table_cleanup(struct table *t) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (!t) return;
+
+    for (unsigned long i = 0; i < t->capacity; ++i) {
+        struct node *cur = t->array[i];
+        while (cur) {
+            struct node *temp = cur;
+            cur = cur->next;
+
+            free(temp->key);
+            array_cleanup(temp->value);
+            free(temp);
+        }
+    }
+
+    free(t->array);
+    free(t);
 }
